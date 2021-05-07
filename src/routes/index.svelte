57ce535815e461248aff244a7698d8231501484f
Th1nkK1D <witheep@gmail.com>
2021-05-07 22:14:30 +0700

    Add windicss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-xl font-bold text-orange-600">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
