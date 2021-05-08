d325cef0cae7bfcd604446b4e58b749993385170
Th1nkK1D <witheep@gmail.com>
2021-05-08 18:29:29 +0700

    Connect with covid th API

---
+++ src/routes/th-covid-speech/_chart.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { DailyNewCase } from './index.svelte';
+
+	export let dailyNewCases: DailyNewCase[];
+</script>
+
+<div>
+	{dailyNewCases}
+</div>
--- src/routes/th-covid-speech/index.svelte
+++ src/routes/th-covid-speech/index.svelte
@@ -1,5 +1,59 @@
-<script>
+<script lang="ts" context="module">
+	interface CovidData {
+		Confirmed: number;
+		Date: string;
+		Deaths: number;
+		Hospitalized: number;
+		NewConfirmed: number;
+		NewDeaths: number;
+		NewHospitalized: number;
+		NewRecovered: number;
+		Recovered: number;
+	}
+
+	interface CovidResponse {
+		Data: CovidData[];
+		DevBy: string;
+		SeverBy: string;
+		Source: string;
+		UpdateDate: string;
+	}
+
+	export interface DailyNewCase {
+		date: Date;
+		count: number;
+	}
+
+	export async function load({ fetch }) {
+		const res = await fetch('https://covid19.th-stat.com/api/open/timeline');
+
+		if (res.ok) {
+			const { Data } = (await res.json()) as CovidResponse;
+
+			const dailyNewCases: DailyNewCase[] = Data.map(({ Date: date, NewConfirmed }) => ({
+				date: new Date(date),
+				count: NewConfirmed
+			}));
+
+			return {
+				props: {
+					dailyNewCases
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load Covid API`)
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Chart from './_chart.svelte';
 	import Content from './_content.svelte';
+
+	export let dailyNewCases: DailyNewCase[];
 </script>
 
 <svelte:head>
@@ -11,7 +65,9 @@
 		<div class="flex p-4 -sm:(justify-center p-3)">
 			<a href="/"><img src="logo-white.png" alt="OrdinaryUnfold" class="h-6 md:h-8" /></a>
 		</div>
-		<div class="flex-1">Chart</div>
+		<div class="flex-1">
+			<Chart {dailyNewCases} />
+		</div>
 	</div>
 	<div class="w-full max-w-2xl">
 		<Content />
