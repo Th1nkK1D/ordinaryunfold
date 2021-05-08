1ce8f71d64b5b4389f158a06ba986f32652498fd
Th1nkK1D <witheep@gmail.com>
2021-05-08 13:15:03 +0700

    Add static build adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import WindiCSS from 'vite-plugin-windicss';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +11,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter(),
 		vite: {
 			plugins: [WindiCSS.default()]
 		}
