6391f8be3eccf044e7b3afecc21859f6b0b48efa
Th1nkK1D <witheep@gmail.com>
2021-05-08 11:50:32 +0700

    Setup typegraphy

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,4 +2,18 @@
 	import 'virtual:windi.css';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,400;0,700;1,400&family=Prompt:ital,wght@0,400;0,700;1,400&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <slot />
+
+<style>
+	:global(body) {
+		@apply font-body;
+	}
+</style>
---
+++ windi.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'windicss/helpers';
+
+export default defineConfig({
+	theme: {
+		fontFamily: {
+			head: ['Prompt', 'sans-serif'],
+			body: ['Bai Jamjuree', 'serif']
+		}
+	}
+});
