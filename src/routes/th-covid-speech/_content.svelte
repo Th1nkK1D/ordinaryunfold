0a17c924739c223e7bf3b665746cf4701a7e2f71
Th1nkK1D <witheep@gmail.com>
2021-05-08 17:43:14 +0700

    Add th covid speech page layout

---
+++ src/routes/th-covid-speech/_content.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-col py-6 px-4 md:(py-12 px-10)">
+	<div class="space-y-4 md:space-y-8">
+		<h1 class="font-head font-bold text-2xl md:text-5xl">
+			ในวิกฤติกาลโควิด 19 <br />รัฐบาลไทยได้พูดอะไรไว้บ้าง?
+		</h1>
+		<div class="h-1 md:h-1.5 w-16 bg-blue-gray-900" />
+		<p class="text-right font-head italic">อัพเดตล่าสุด ...</p>
+	</div>
+</div>
---
+++ src/routes/th-covid-speech/index.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Content from './_content.svelte';
+</script>
+
+<svelte:head>
+	<title>Thai Government COVID19 Speeches - OrdinaryUnfold</title>
+</svelte:head>
+
+<div class="flex flex-col md:flex-row w-screen h-screen">
+	<div class="-sm:h-72 md:flex-1 bg-blue-gray-900 flex flex-col">
+		<div class="flex p-4 -sm:(justify-center p-3)">
+			<a href="/"><img src="logo-white.png" alt="OrdinaryUnfold" class="h-6 md:h-8" /></a>
+		</div>
+		<div class="flex-1">Chart</div>
+	</div>
+	<div class="w-full max-w-2xl">
+		<Content />
+	</div>
+</div>
