660d655a3254ea76c1262493dfcabaac1ae70116
Th1nkK1D <witheep@gmail.com>
2021-05-14 15:32:29 +0700

    Update landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,34 @@
 <script>
 	import Metadata from '../components/metadata.svelte';
 	import Footer from '../components/footer.svelte';
+
 </script>
 
 <Metadata />
 
 <div
-	class="w-full min-h-screen bg-cool-gray-900 text-white flex-1 flex flex-col justify-center items-center m-auto space-y-8 text-center px-3 py-18"
+	class="w-full min-h-screen bg-cool-gray-900 text-white flex-1 flex flex-col justify-center items-center m-auto space-y-12 text-center px-3 py-18"
 >
-	<svg class="w-24 md:w-32" viewBox="0 0 45 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M35.0261 7.25519C35.0261 7.70765 34.8014 8.29327 34.064 8.98587C33.3238 9.68105 32.1717 10.3762 30.6232 10.9914C27.5347 12.2183 23.1654 13.0104 18.2631 13.0104C13.3608 13.0104 8.9914 12.2183 5.90292 10.9914C4.35436 10.3762 3.20227 9.68105 2.46212 8.98587C1.7247 8.29327 1.5 7.70765 1.5 7.25519C1.5 6.80272 1.7247 6.2171 2.46212 5.5245C3.20227 4.82933 4.35436 4.1342 5.90292 3.51902C8.9914 2.29209 13.3608 1.5 18.2631 1.5C23.1654 1.5 27.5347 2.29209 30.6232 3.51902C32.1717 4.1342 33.3238 4.82933 34.064 5.5245C34.8014 6.2171 35.0261 6.80272 35.0261 7.25519Z"
-			stroke="white"
-			stroke-width="3"
-		/>
-		<ellipse cx="18.2631" cy="7.25521" rx="6.25447" ry="2.25161" fill="white" />
-		<line x1="35.0261" y1="15.0107" x2="35.0261" y2="7.00502" stroke="white" stroke-width="3" />
-		<path
-			d="M41 15.7984C17.9982 19.9561 5.83884 13.6154 1.5 10V38C8.20439 47.8338 34.0417 45.5928 40.4608 42.7005L36.1685 38.5399L40.4608 33.6939L36.8839 30.3017L41 26.9095L36.1685 22.3058L41 15.7984Z"
-			stroke="white"
-			stroke-width="3"
-		/>
-	</svg>
+	<img src="/logo-white.png" alt="OrdinaryUnfold" class="w-full max-w-96" />
 
-	<h1 class="text-4xl md:text-5xl font-head font-bold">OrdinaryUnfold</h1>
-	<h3 class="text-2xl font-bold">Bite-size visualization and storytelling experiments</h3>
+	<div class="space-y-4">
+		<div class="flex">
+			<a
+				href="/th-covid-speech"
+				class="mx-auto transform transition-transform duration-200 ease-in-out hover:(scale-102 shadow-xl)"
+			>
+				<img
+					src="/th-covid-speech/og.png"
+					class="rounded-lg shadow-lg w-full max-w-86"
+					alt="วิวาทะโควิด 19"
+				/>
+			</a>
+		</div>
+		<p class="max-w-screen-sm mx-auto">
+			We just launch first experimental story about Thailand Covid-19 related speeches [TH],<br />so
+			landing page will be redesigned later.
+		</p>
+	</div>
 
-	<p class="max-w-screen-md mx-auto">
-		We just launch first experimental story about Thailand Covid-19 related speeches<br />(sorry
-		folks, it is written in Thai)
-	</p>
-
-	<a
-		href="/th-covid-speech"
-		class="w-full max-w-92 rounded-lg overflow-hidden shadow-lg mx-auto transform transition-transform duration-200 ease-in-out hover:(scale-102 shadow-xl)"
-	>
-		<img src="/th-covid-speech/og.png" class="" alt="วิวาทะโควิด 19" />
-	</a>
-
-	<Footer />
+	<Footer class="opacity-50" />
 </div>
