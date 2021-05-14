04bd10554ecd2d2df9d247b2d7bdeb91106191ad
Th1nkK1D <witheep@gmail.com>
2021-05-14 16:44:34 +0700

    Add author name in footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,3 +1,7 @@
 <footer class="text-sm {$$props.class || ''}">
-	Copyright &copy; {new Date().getFullYear()} OrdinaryUnfold
+	Crafted by
+	<a href="https://th1nkk1d.xyz" rel="noreferrer noopener" target="_blank" class="hover:underline"
+		>Th1nkK1D</a
+	>
+	- Copyright &copy; {new Date().getFullYear()} OrdinaryUnfold
 </footer>
