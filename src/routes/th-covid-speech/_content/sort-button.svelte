95b955c4344ee00d3f080e7317bfb634a4d82c6b
Th1nkK1D <witheep@gmail.com>
2021-05-25 21:05:38 +0700

    Add sort button border

--- src/routes/th-covid-speech/_content/sort-button.svelte
+++ src/routes/th-covid-speech/_content/sort-button.svelte
@@ -5,7 +5,10 @@
 
 </script>
 
-<button class="pl-2 pr-3 py-1 text-sm text-light-blue-700 hover:bg-gray-100 rounded" on:click>
+<button
+	class="pl-2 pr-3 py-1 text-sm text-light-blue-700 hover:bg-gray-100 rounded border"
+	on:click
+>
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 inline fill-current">
 		<path class={isReversed ? 'opacity-20' : ''} d="M16 17.01V10h-2v7.01h-3L15 21l4-3.99h-3z" />
 		<path class={isReversed ? '' : 'opacity-20'} d="M9 3L5 6.99h3V14h2V6.99h3L9 3z" />
