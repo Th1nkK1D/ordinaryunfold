9079aa19f0d01159aa84a1dd8d390c2b04163256
Th1nkK1D <witheep@gmail.com>
2021-07-03 19:24:35 +0700

    Add fade back when exit chapter step

--- src/routes/myanmar-coup/_sections/chapter.svelte
+++ src/routes/myanmar-coup/_sections/chapter.svelte
@@ -12,24 +12,22 @@
 
 	export let steps: Step[] = [];
 	export let activePeopleMask: boolean[];
+
+	const onStepEnter = ({ detail: { index } }) => {
+		activePeopleMask = index === 0 || index > steps.length ? [] : steps[index - 1].matchedMask;
+	};
 </script>
 
-<section class="p-8 md: p-20">
+<Scrollama class="p-8 md: p-20" on:stepenter={onStepEnter}>
 	<div class="flex flex-col justify-center min-h-screen">
 		<slot />
 	</div>
 
-	{#if steps.length > 0}
-		<Scrollama
-			on:stepenter={({ detail: { index } }) => {
-				activePeopleMask = steps[index].matchedMask;
-			}}
-		>
-			{#each steps as { heading, matchedMask }}
-				<div class="flex flex-col justify-center h-25vh">
-					<p class="text-white">{heading(matchedMask.filter((value) => value).length)}</p>
-				</div>
-			{/each}
-		</Scrollama>
-	{/if}
-</section>
+	{#each steps as { heading, matchedMask }}
+		<div class="flex flex-col justify-center h-50vh">
+			<p class="text-white">{heading(matchedMask.filter((value) => value).length)}</p>
+		</div>
+	{/each}
+
+	<div class="h-50vh" />
+</Scrollama>
