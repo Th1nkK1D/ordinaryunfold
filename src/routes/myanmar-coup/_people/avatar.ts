2b6e6214e9a046de4cff20f35e7d3a403ef05170
Th1nkK1D <witheep@gmail.com>
2021-07-03 17:33:52 +0700

    Simplify avatar shape for performance

--- src/routes/myanmar-coup/_people/avatar.ts
+++ src/routes/myanmar-coup/_people/avatar.ts
@@ -1,45 +1,48 @@
 import paper from 'paper/dist/paper-core';
-import { BACK_HAIR, FACES, FRONT_HAIR } from './variation.json';
 
 export const BASE_SIZE = 100;
 
+const FACE_VARIATION = [10, 15, 20];
+
 const DEFAULT_COLOR = new paper.Color('#404040');
-const BG_COLOR = new paper.Color('#171717');
 
-export const drawAvatar = (backHair: string[], face: string, frontHair: string): paper.Group => {
-	const paths = [
-		...backHair.map((d) => new paper.Path(d)),
-		new paper.Path(face),
-		new paper.Path(frontHair)
-	];
-
-	paths.forEach((path) => {
-		path.fillColor = BG_COLOR;
-		path.strokeColor = DEFAULT_COLOR;
-		path.strokeWidth = 4;
-		path.strokeCap = 'round';
+export const drawAvatar = (y: number, xTop: number, xButtom: number): paper.Group => {
+	const face = new paper.Path({
+		segments: [
+			new paper.Point(xTop, y),
+			new paper.Point(100 - xTop, y),
+			new paper.Point(100 - xButtom, 100 - y),
+			new paper.Point(xButtom, 100 - y)
+		],
+		closed: true,
+		strokeColor: DEFAULT_COLOR,
+		strokeWidth: 4
 	});
 
 	const eyes = [65.5, 35.5].map(
 		(x) =>
-			new paper.Shape.Ellipse({
-				center: [x, 59.5],
-				radius: [2.5, 7.5],
-				fillColor: DEFAULT_COLOR
+			new paper.Path({
+				segments: [
+					[x, 45],
+					[x, 55]
+				],
+				strokeColor: DEFAULT_COLOR,
+				strokeWidth: 8,
+				strokeCap: 'round'
 			})
 	);
 
-	return new paper.Group([...paths, ...eyes]);
+	return new paper.Group([face, ...eyes]);
 };
 
 export const generateAvatarSymbols = (): paper.SymbolDefinition[] => {
 	const symbols: paper.SymbolDefinition[] = [];
 
-	BACK_HAIR.forEach((backHair) =>
-		FACES.forEach((face) =>
-			FRONT_HAIR.forEach((frontHair) => {
-				symbols.push(new paper.SymbolDefinition(drawAvatar(backHair, face, frontHair)));
-			})
+	FACE_VARIATION.forEach((y) =>
+		FACE_VARIATION.forEach((xTop) =>
+			FACE_VARIATION.forEach((xBottom) =>
+				symbols.push(new paper.SymbolDefinition(drawAvatar(y, xTop, xBottom)))
+			)
 		)
 	);
 
