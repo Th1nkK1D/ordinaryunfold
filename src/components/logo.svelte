76b0998a1e6b9bfb62a3dd17209aef40f04a05c6
Th1nkK1D <witheep@gmail.com>
2021-12-27 22:25:49 +0200

    Add whats in the city page

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,1 +1,11 @@
-<a href="/"><img src="/images/logo-white.png" alt="OrdinaryUnfold" class="h-6 md:h-8" /></a>
+<script lang="ts">
+	export let dark = false;
+</script>
+
+<a href="/"
+	><img
+		src="/images/logo-{dark ? 'black' : 'white'}.png"
+		alt="OrdinaryUnfold"
+		class="h-6 md:h-8"
+	/></a
+>
---
+++ src/routes/whats-in-the-city/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Logo from '../../components/logo.svelte';
+</script>
+
+<div class="flex flex-col w-screen h-screen p-8">
+	<div>
+		<Logo dark />
+	</div>
+	<div class="flex flex-row flex-1 space-x-8">
+		<div class="font-head font-bold mt-48 space-y-2">
+			<h1 class="text-4xl">What's in</h1>
+			<h2 class="text-6xl">Bangkok</h2>
+		</div>
+		<div class="flex-1 bg-gray-200" />
+	</div>
+</div>
