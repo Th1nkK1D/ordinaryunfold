3e23de7c970c7883c124812bd56c584bd94ad87e
Th1nkK1D <witheep@gmail.com>
2021-12-29 21:57:46 +0200

    Plot map into paperjs canvas instead of svg

--- src/routes/whats-in-the-city/_map/map.svelte
+++ src/routes/whats-in-the-city/_map/map.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { geoPath, geoMercator } from 'd3-geo';
+	import paper from 'paper';
 
 	import bangkokGeo from '../../../data/whats-in-the-city/bangkok-geo.json';
 
-	const projection = geoMercator().fitExtent(
-		[
-			[0, 0],
-			[940, 480]
-		],
-		bangkokGeo
-	);
-	const path = geoPath(projection);
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		paper.setup(canvas);
+
+		const { clientWidth, clientHeight } = canvas;
+
+		const projection = geoMercator().fitExtent(
+			[
+				[0, 0],
+				[clientWidth, clientHeight]
+			],
+			bangkokGeo
+		);
+
+		const path = geoPath(projection);
+
+		const cityPath = new paper.Path(path(bangkokGeo));
+		cityPath.strokeColor = 'black';
+	});
 </script>
 
-<svg class="h-full w-full">
-	<path d={path(bangkokGeo)} />
-</svg>
+<canvas class="h-full w-full" bind:this={canvas} />
