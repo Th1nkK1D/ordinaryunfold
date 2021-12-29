88b587a09b4d60a185f796a6761eed98745a20df
Th1nkK1D <witheep@gmail.com>
2021-12-29 23:07:40 +0200

    Calculate pixel per km for point radius

--- src/routes/whats-in-the-city/_map/map.svelte
+++ src/routes/whats-in-the-city/_map/map.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { geoPath, geoMercator, geoCircle } from 'd3-geo';
+	import { geoPath, geoMercator } from 'd3-geo';
+	import destination from '@turf/destination';
+	import { lineString } from '@turf/helpers';
 	import paper from 'paper';
 
 	import bangkokGeo from '../../../data/whats-in-the-city/bangkok-geo.json';
 	import bangkokGarden from '../../../data/whats-in-the-city/bangkok-garden.json';
 
 	let canvas: HTMLCanvasElement;
+
+	const POINT_RADIUS = [2, 5, 10];
 
 	onMount(() => {
 		paper.setup(canvas);
@@ -26,9 +30,14 @@
 		const cityPath = new paper.Path(path(bangkokGeo));
 		cityPath.fillColor = '#E5E5E5';
 
+		const centroid = path.centroid(bangkokGeo);
+		const pixelPerKm = path.measure(
+			lineString([centroid, destination(centroid, 1, 0).geometry.coordinates as [number, number]])
+		);
+
 		bangkokGarden.forEach(({ lat, lon }) => {
 			const point = new paper.Point(projection([lon, lat]));
-			const circle = new paper.Shape.Circle(point, 5);
+			const circle = new paper.Shape.Circle(point, POINT_RADIUS[0] * pixelPerKm);
 			circle.fillColor = 'green';
 		});
 	});
