00a6c1b0e2308582a7691c8e2d22d53bb7a54acb
Th1nkK1D <witheep@gmail.com>
2022-01-10 23:42:18 +0200

    Add generate place stats script

---
+++ src/data/whats-in-the-city/generate-places-stats.ts
@@ -0,0 +1,37 @@
+const INPUT_PATH = '../../../static/whats-in-the-city/json/';
+const OUTPUT_FILE = 'places-stats.json';
+
+const placeFiles = [...Deno.readDirSync(INPUT_PATH)]
+	.filter(({ name }) => !name.includes('-geo'))
+	.map(({ name }) => {
+		const [city, ...category] = name.replace('.json', '').split('-');
+		return {
+			city,
+			category: category.join('-'),
+			path: `${INPUT_PATH}${name}`
+		};
+	});
+
+placeFiles.sort((a, z) => a.category.localeCompare(z.category));
+
+const contentCounts = (
+	await Promise.all(placeFiles.map(({ path }) => Deno.readTextFile(path)))
+).map((text) => JSON.parse(text).length);
+
+const stats = placeFiles.reduce<{
+	[city: string]: {
+		category: string;
+		count: number;
+	}[];
+}>((obj, { city, category }, index) => {
+	if (!obj[city]) obj[city] = [];
+
+	obj[city].push({
+		category,
+		count: contentCounts[index]
+	});
+
+	return obj;
+}, {});
+
+Deno.writeTextFile(OUTPUT_FILE, JSON.stringify(stats));
