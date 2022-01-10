29f7f01ffa4e34e2d728f392e130663f3a178aec
Th1nkK1D <witheep@gmail.com>
2022-01-10 23:09:06 +0200

    Add area vis in comparison

--- src/routes/whats-in-the-city/_components/comparison.svelte
+++ src/routes/whats-in-the-city/_components/comparison.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+	import { scaleLinear } from 'd3-scale';
 	import cities from '../../../data/whats-in-the-city/cities.json';
 
 	const MIN_COLUMN_WIDTH = 180;
 	const POPULATION_PER_CIRCLE = 100000;
+	const MAX_AREA_SIZE = 120;
+
+	const populationVis = cities.map(({ population }) => {
+		const amount = population / POPULATION_PER_CIRCLE;
+		const integer = Math.floor(amount);
+		return { population, integer, fraction: amount - integer };
+	});
+
+	const mapArea = scaleLinear()
+		.domain([0, Math.max(...cities.map(({ area }) => area))])
+		.range([0, MAX_AREA_SIZE]);
+
+	const areaVis = cities.map(({ area }) => ({
+		area,
+		size: Math.round(mapArea(area))
+	}));
 </script>
 
 <div
@@ -17,11 +34,7 @@
 
 	<div class="row-label">Population</div>
 
-	{#each cities.map(({ population }) => {
-		const amount = population / POPULATION_PER_CIRCLE;
-		const integer = Math.floor(amount);
-		return { population, integer, fraction: amount - integer };
-	}) as { population, integer, fraction }}
+	{#each populationVis as { population, integer, fraction }}
 		<div class="cell space-y-1">
 			<div class="flex flex-row flex-wrap content-start">
 				{#each Array(Math.floor(integer)) as _}
@@ -33,6 +46,15 @@
 				</div>
 			</div>
 			<p class="text-center text-sm">{population.toLocaleString()}</p>
+		</div>
+	{/each}
+
+	<div class="row-label">Area</div>
+
+	{#each areaVis as { area, size }}
+		<div class="cell space-y-1 flex flex-col justify-center items-center">
+			<div class="bg-green-300" style="width: {size}px; height: {size}px;" />
+			<p class="text-center text-sm">{area.toLocaleString()} km<sup>2</sup></p>
 		</div>
 	{/each}
 </div>
@@ -47,6 +69,6 @@
 	}
 
 	.population-dot {
-		@apply w-2 h-2 rounded-full bg-blue-600;
+		@apply w-2 h-2 rounded-full bg-blue-500;
 	}
 </style>
