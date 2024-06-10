ceb8025e2edd72f98d483f7837f207a7bcb4d222
Th1nkK1D <witheep@gmail.com>
2024-06-10 21:07:59 +0700

    feat(cbking): add team ranking to the navigation

--- src/routes/comeback-king-or-a-flopper/_components/team-navigation.svelte
+++ src/routes/comeback-king-or-a-flopper/_components/team-navigation.svelte
@@ -11,13 +11,13 @@
 			<svg viewBox="0 0 24 24">
 				<path d="M14 17.308L8.692 12L14 6.692l.708.708l-4.6 4.6l4.6 4.6z" />
 			</svg>
-			<span class="text-left">{teams[teamIndex - 1].name}</span>
+			<span class="text-left">{teamIndex}. {teams[teamIndex - 1].name}</span>
 		</button>
 	{/if}
 	<div class="min-w-6 flex-1" />
 	{#if teamIndex < teams.length - 1}
 		<button on:click={() => teamIndex++}>
-			<span class="text-right">{teams[teamIndex + 1].name}</span>
+			<span class="text-right">{teamIndex + 2}. {teams[teamIndex + 1].name}</span>
 			<svg viewBox="0 0 24 24">
 				<path d="m13.292 12l-4.6-4.6l.708-.708L14.708 12L9.4 17.308l-.708-.708z" />
 			</svg>
