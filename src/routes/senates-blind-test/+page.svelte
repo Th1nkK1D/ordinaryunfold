99521a9526384158c99ef55098c6a473a6640703
Th1nkK1D <witheep@gmail.com>
2024-07-02 22:56:37 +0700

    feat(senates): add footer section

--- src/routes/senates-blind-test/+page.svelte
+++ src/routes/senates-blind-test/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import ExternalLink from '../../components/external-link.svelte';
+	import Footer from '../../components/footer.svelte';
 	import Metadata from '../../components/metadata.svelte';
+	import Sharer from '../../components/sharer.svelte';
 	import IntroScene from './_components/intro-scene.svelte';
 	import QuizScene from './_components/quiz-scene.svelte';
 
@@ -17,17 +20,39 @@
 	path="/senates-blind-test"
 />
 
-<div
-	class="relative min-h-lvh border-[6px] transition-colors duration-500 md:border-8 {isGameStarted
-		? 'border-gray-400 bg-gray-200'
-		: 'border-white/15 bg-[#2B815F]'}"
->
+<div class="relative min-h-lvh">
 	{#if isGameStarted && candidates}
 		<QuizScene {candidates} {groups} {choiceLabels} bind:isGameStarted />
 	{:else}
 		<IntroScene {choiceLabels} on:start={() => (isGameStarted = true)} />
 	{/if}
 </div>
+
+<section class="bg-gray-800 text-gray-100">
+	<div
+		class="mx-auto flex w-full max-w-screen-lg flex-col items-center justify-between gap-6 px-3 py-8 md:flex-row-reverse md:py-12"
+	>
+		<div class="flex items-center justify-center gap-3 md:gap-5">
+			<span class="text-xs text-gray-300 md:text-sm">Share</span>
+			<Sharer
+				url="https://ordinaryunfold.com/senates-blind-test"
+				linkClass="text-gray-300 hover:text-gray-100"
+			/>
+		</div>
+		<div class="flex flex-col gap-6 text-center md:text-left">
+			<p class="md:text-md text-sm">
+				ข้อมูลผู้สมัครสว. จาก <ExternalLink
+					href="https://github.com/PanJ/senate67"
+					class="underline">PanJ/senate67</ExternalLink
+				> และข้อมูล <ExternalLink
+					href="https://docs.google.com/spreadsheets/d/1zJSO-l1nXAaj0g9YBoCzZrEyunBlRoHFiGsrN2fzDsQ/edit"
+					class="underline">ผลคะแนนดิบสว.67 รอบระดับประเทศจาก iLaw</ExternalLink
+				>
+			</p>
+			<Footer class="text-gray-100" />
+		</div>
+	</div>
+</section>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Charmonman:wght@700&family=Chonburi&display=swap');
--- src/routes/senates-blind-test/_components/intro-scene.svelte
+++ src/routes/senates-blind-test/_components/intro-scene.svelte
@@ -9,42 +9,43 @@
 	const dispatch = createEventDispatcher();
 </script>
 
-<div
-	class="absolute inset-0 bottom-16 z-10 flex flex-col items-center justify-around overflow-hidden px-3 py-4 text-white md:bottom-24 md:gap-12 md:py-12"
->
-	<Logo />
-	<div class="flex flex-col gap-4 text-center md:gap-6">
-		<h1 class="font-chonburi text-4xl md:text-7xl">Senates Blind Test</h1>
-		<p class="font-charmonman text-xl md:text-3xl">
-			เกมทายใจ ใครเป็นผู้ชนะการเลือกตั้งสว. จากเอกสารแนะนำตัว ๕ บรรทัด
-		</p>
-	</div>
-	<div class="flex max-w-screen-md flex-col gap-2 indent-8">
-		<p>
-			จากผู้สมัครทั้งหมด ๘๐๐ คน ในการเลือกตั้งสว. รอบสุดท้ายของปี ๒๕๖๗
-			ผู้ที่ได้คะแนนโหวตจากสว.ด้วยกันเองสูงสุด ๑๐ คนในแต่ละกลุ่มอาชีพ ๒๐ กลุ่ม (รวมเป็น ๒๐๐ คน)
-			จะได้รับการแต่งตั้งเป็นสว. <a
-				href="https://senates67.com"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="underline opacity-60">(อ่านเพิ่มเติมเกี่ยวกับระบบการเลือกตั้งสว.)</a
-			>
-		</p>
-		<p>
-			เกม "สว. Blind Test จะสุ่มกลุ่มอาชีพพร้อมกับ
-			"ประวัติการทํางานหรือประสบการณ์ในการทํางานในกลุ่มที่สมัคร (ไม่เกิน ๕ บรรทัด)" จากใบสมัครของ
-			สว. ๔ คน ผู้เล่นต้องทายว่าคนที่ได้รับเลือกเป็นสว. ๑ ใน ๔ คนนั้นคือใคร?
-		</p>
-		<p class="text-sm opacity-60">
-			หมายเหตุ: เราใช้เทคนิค OCR ในการแปลงรูปใบสมัครต้นฉบับให้หลายเป็นตัวหนังสือ
-			อาจมีข้อมูลที่คลาดเคลื่อน สามารถรายงานความผิดพลาดได้ แต่เราจะไม่รับผิดชอบใดๆ
-		</p>
-	</div>
-	<Button class="z-50 bg-white text-[#2B815F] shadow-lg" on:click={() => dispatch('start')}
-		>เริ่มเล่น</Button
+<div class="relative h-lvh w-full overflow-hidden">
+	<div
+		class="absolute inset-0 flex flex-col items-center justify-around overflow-hidden border-[8px] border-white/15 bg-[#2B815F] px-2 pb-12 pt-4 text-white transition-colors duration-500 md:gap-12 md:border-[12px] md:pb-36 md:pt-12"
 	>
-
-	<div class="fixed bottom-0 left-1/2 flex w-[60vw] -translate-x-1/2 flex-row md:max-w-xl">
+		<Logo />
+		<div class="flex flex-col gap-4 text-center md:gap-6">
+			<h1 class="font-chonburi text-4xl md:text-7xl">Senates Blind Test</h1>
+			<p class="font-charmonman text-xl md:text-3xl">
+				เกมทายใจ ใครเป็นผู้ชนะการเลือกตั้งสว. จากเอกสารแนะนำตัว ๕ บรรทัด
+			</p>
+		</div>
+		<div class="flex max-w-screen-md flex-col gap-2 indent-8">
+			<p>
+				จากผู้สมัครทั้งหมด ๘๐๐ คน ในการเลือกตั้งสว. รอบสุดท้ายของปี ๒๕๖๗
+				ผู้ที่ได้คะแนนโหวตจากสว.ด้วยกันเองสูงสุด ๑๐ คนในแต่ละกลุ่มอาชีพ ๒๐ กลุ่ม (รวมเป็น ๒๐๐ คน)
+				จะได้รับการแต่งตั้งเป็นสว. <a
+					href="https://senates67.com"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="underline opacity-60">(อ่านเพิ่มเติมเกี่ยวกับระบบการเลือกตั้งสว.)</a
+				>
+			</p>
+			<p>
+				เกม "สว. Blind Test จะสุ่มกลุ่มอาชีพพร้อมกับ
+				"ประวัติการทํางานหรือประสบการณ์ในการทํางานในกลุ่มที่สมัคร (ไม่เกิน ๕ บรรทัด)" จากใบสมัครของ
+				สว. ๔ คน ผู้เล่นต้องทายว่าคนที่ได้รับเลือกเป็นสว. ๑ ใน ๔ คนนั้นคือใคร?
+			</p>
+			<p class="text-sm opacity-60">
+				หมายเหตุ: เราใช้เทคนิค OCR ในการแปลงรูปใบสมัครต้นฉบับให้หลายเป็นตัวหนังสือ
+				อาจมีข้อมูลที่คลาดเคลื่อน สามารถรายงานความผิดพลาดได้ แต่เราจะไม่รับผิดชอบใดๆ
+			</p>
+		</div>
+		<Button class="z-50 bg-white text-[#2B815F] shadow-lg" on:click={() => dispatch('start')}
+			>เริ่มเล่น</Button
+		>
+	</div>
+	<div class="absolute bottom-0 left-1/2 flex w-[60vw] -translate-x-1/2 flex-row md:max-w-xl">
 		{#each choiceLabels as label, i}
 			<div class="relative flex-1 transition-transform hover:-translate-y-2">
 				<Card
