2786bf39aa01d6737ead2ad997d7cba3fbcb0faf
Th1nkK1D <witheep@gmail.com>
2026-01-04 22:03:49 +0700

    chore: migrate bun text lockfile and add vite chrome dev tools plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import dsv from '@rollup/plugin-dsv';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [sveltekit(), dsv()]
+	plugins: [sveltekit(), dsv(), devtoolsJson()]
 });
